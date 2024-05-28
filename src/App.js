29e971bfcf893c7253f489efa3c7e9e6bbc43c96
Niikkc <65796016+Niikkc@users.noreply.github.com>
2024-05-27 23:02:48 -0700

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { NavBar } from './components/NavBar';
+import Hero from './components/Hero';
+import About from './components/About';
+import EducationSkills from './components/EducationSkills';
+import ExperienceProjects from './components/ExperienceProjects';
+import Extracurriculars from './components/Extracurriculars';
+import Footer from './components/Footer';
+import Banner from './components/Banner'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import './snow.scss';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <Hero />
+      <About />
+      <EducationSkills />
+      <ExperienceProjects />
+      <Extracurriculars />
+      <Footer />
+
     </div>
   );
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
